36cbeb4977be2bb0901e77af5a96799c2ec83f94
dubtsov <al.dubtsov@gmail.com>
2024-03-29 08:59:01 +0200

    total number in print header

--- textPrepare/stringForPrint.go
+++ textPrepare/stringForPrint.go
@@ -2,34 +2,47 @@ package textPrepare
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/AlexDubtsov/SD_2024_public/m/v2/structures"
 )
 
 func MalePrint(templateData *structures.Template_Basic_EditEvent) string {
 	var males []structures.Basic_SinglePerson
-
-	initString := templateData.Name + "\t" + templateData.Date + "\t" + "MALE" + "\n\n"
+	numberParticipants := 0
+	var numberParticipantsString string
 
 	for i := range templateData.Slice_Participants {
 		if templateData.Slice_Participants[i].Gender == "Male" {
 			males = append(males, templateData.Slice_Participants[i])
+			numberParticipants++
 		}
 	}
+
+	numberParticipantsString = strconv.Itoa(numberParticipants)
+
+	initString := templateData.Name + "\t" + templateData.Date + "\t" + "MALE" + "\t" + "Total " + numberParticipantsString + "\n\n"
+
 	stringForPrint := initString + printTablePrepare(males)
 	return stringForPrint
 }
 
 func FemalePrint(templateData *structures.Template_Basic_EditEvent) string {
 	var females []structures.Basic_SinglePerson
-
-	initString := templateData.Name + "\t" + templateData.Date + "\t" + "FEMALE" + "\n\n"
+	numberParticipants := 0
+	var numberParticipantsString string
 
 	for i := range templateData.Slice_Participants {
 		if templateData.Slice_Participants[i].Gender == "Female" {
 			females = append(females, templateData.Slice_Participants[i])
+			numberParticipants++
 		}
 	}
+
+	numberParticipantsString = strconv.Itoa(numberParticipants)
+
+	initString := templateData.Name + "\t" + templateData.Date + "\t" + "FEMALE" + "\t" + "Total " + numberParticipantsString + "\n\n"
+
 	stringForPrint := initString + printTablePrepare(females)
 	return stringForPrint
 }
